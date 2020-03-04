0b51aa0511c3164ea94b472a9f53adb207983fec
Fredrik Fornwall <fredrik@fornwall.net>
2020-03-04 15:20:37 +0200

    Adopt to theming a little

--- meeshkan-panel.js
+++ meeshkan-panel.js
@@ -1,7 +1,11 @@
+counter = 0;
+
 chrome.devtools.network.onRequestFinished.addListener(request => {
   request.getContent((body) => {
     if (request.request && request.request.url) {
       console.log(body);
+      document.getElementById('info').textContent = counter++;
+ 
       //if (request.request.url.includes('<url-to-intercept>')) {
         //chrome.runtime.sendMessage({
             //response: body
